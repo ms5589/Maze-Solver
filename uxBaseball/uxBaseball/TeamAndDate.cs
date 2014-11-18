d60320db4a0eeddce7b1b1d99979896644baf0d6
sagar5589 <sagar5589@outlook.com>
2014-11-18 10:27:16 -0600

    Project_5

---
+++ uxBaseball/uxBaseball/TeamAndDate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uxBaseball
+{
+    struct TeamAndDate
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private DateTime date;
+        /// <summary>
+        /// 
+        /// </summary>
+        private string abbr;
+        /// <summary>
+        /// 
+        /// </summary>
+        private int hashCode;
+        
+        //stucture(2 param) YMD
+        public TeamAndDate(string s, string time)
+        {
+            int year = Convert.ToInt32(time.Substring(0, 4));
+            int month = Convert.ToInt32(time.Substring(4, 2));
+            int day = Convert.ToInt32(time.Substring(6, 2));
+            date = new DateTime(year, month, day);
+            abbr = s;
+            unchecked { 
+            hashCode = Convert.ToInt32(abbr[0]);
+            hashCode *= 39;
+            hashCode += Convert.ToInt32(abbr[1]);
+            hashCode *= 39;
+            hashCode += Convert.ToInt32(abbr[2]);
+            hashCode *= 39;
+            hashCode += date.Year;
+            hashCode *= 39;
+            hashCode += date.Month;
+            hashCode *= 39;
+            hashCode += date.Day;
+            hashCode *= 39;
+            }
+            //throw new Exception();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return hashCode;
+        }
+        public static bool operator ==(TeamAndDate x, TeamAndDate y)
+        {
+            if (x.date == y.date && x.abbr == y.abbr) return true;
+            else return false;
+        }
+        public static bool operator !=(TeamAndDate x, TeamAndDate y)
+        {
+            return!(x==y);
+        }
+    }
+}
---
+++ uxBaseball/uxBaseball/TeamData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uxBaseball
+{
+    class TeamData: IComparable<TeamData>
+    {
+        public string teamAbbreviation;
+        public string location;
+        public string nickName;
+        public string firstYear;
+        public string lastYear;
+        
+        public TeamData(string[] str)
+        {
+            teamAbbreviation = str[0];
+            location = str[1];
+            nickName = str[2];
+            firstYear= str[3];
+            lastYear = str[4];
+        }
+        public override string ToString()
+        {
+            return (location+" "+nickName+" ("+firstYear+") "+lastYear+" ") ;
+        } //toString override
+        
+        public int CompareTo(TeamData td)
+        {
+            return this.ToString().CompareTo(td.ToString());
+        }
+        
+    }
+}
